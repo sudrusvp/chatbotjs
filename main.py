04a448804e75a1372afb8909746e4d54003cd8fa
Akshay Mhapankar <akshay_mhapankar@persistent.co.in>
2017-02-04 14:06:17 +0530

    feb 4

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from flask import Flask
 from flask import render_template
 from flask import request, url_for
 import json
-import apiai
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -18,18 +17,7 @@ def main_page():
 		
 		return render_template("index.html")	
 
-		#input_text = request.form['input_text']
-
-	elif request.method == 'POST':
-		#return request.form['message']
-		ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
-		req = ai.text_request()
-		req.session_id = "session11"
-		req.query = request.form['message']
-		res = req.getresponse()
-		response_message = res.read()
-		response_message = json.loads(response_message)
-		return response_message["result"]['fulfillment']['speech']
+		#input_text = request.form['input_text'
 	
 if __name__ == "__main__":
 	app.run(debug=True,  port=int(8080))
--- static/js/script.js
+++ static/js/script.js
@@ -10,12 +10,26 @@ $(function(){
 		$(".chatdiv").animate({ scrollTop: $('.chatdiv').prop("scrollHeight")}, 1000);
 		$("#message").val("");
 
-		$.post("/",data,function(res){
+/*		$.post("/",data,function(res){
 			console.log(res)
 			$('.chatdiv').append("<div class='row' style='margin: 5px 0px'> <div class='col-sm-8 text-left'> <div class='received text-left'>"+res+"</div> </div> </div>");
 			$(".chatdiv").animate({ scrollTop: $('.chatdiv').prop("scrollHeight")}, 1000);
 
 		});
+*/
+		const client = new ApiAi.ApiAiClient('2e305571959847228c0c4a5d9a33b4c6');
+		let promise = client.textRequest(data.message);
+
+		promise
+		    .then(handleResponse)
+    		.catch(heandleError);
+
+		function handleResponse(serverResponse) {
+		        console.log(serverResponse);
+		}
+		function heandleError(serverError) {
+		        console.log(serverError);
+		}
 
 	});
 
