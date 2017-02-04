c2496f52087a53c19a87ce2f4a17b42457e87300
akshay <akshay_mhapankar@persistent.com>
2017-02-04 22:35:07 +0530

    c2

--- test.py
+++ test.py
@@ -1,36 +1,17 @@
 
-import os.path
-import sys
-import json
+from flask import Flask
+from flask import render_template
+from flask import request, url_for
 
-try:
-    import apiai
-except ImportError:
-    sys.path.append(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
-    )
-    import apiai
+import MySQLdb
 
-CLIENT_ACCESS_TOKEN = '2e305571959847228c0c4a5d9a33b4c6'
 
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
 def main():
-    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
-
-    request = ai.text_request()
-
-    request.session_id = "session11"
-
-    request.query = "Hello"
-
-    response = request.getresponse()
-
-    result = response.read()
-
-    print "result : ",result
-    print "\ntype of : ",type(result)
-    result = json.loads(result);
-    print "\nresult.result : ",result["result"]['fulfillment']['speech']
+    return "hello"
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.run(debug=True,  port=int(8080))
