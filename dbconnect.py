108e524f7db0da4f57925769fb6f0cca557ca31d
Akshay Mhapankar <akshay_mhapankar@persistent.co.in>
2017-02-04 16:28:23 +0530

    feb 4

---
+++ dbconnect.py
@@ -0,0 +1,32 @@
+
+def connect_to_cloudsql():
+
+	# These environment variables are configured in app.yaml.
+	INSTANCE_CONNECTION_NAME = os.environ.get('INSTANCE_CONNECTION_NAME')
+	CLOUDSQL_USER = os.environ.get('MYSQL_USER')
+	CLOUDSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+	CLOUDSQL_DB = os.environ.get('MYSQL_DATABASE')
+	
+	cloudsql_unix_socket = os.path.join('/cloudsql', INSTANCE_CONNECTION_NAME)
+
+	db = MySQLdb.connect(
+		unix_socket=cloudsql_unix_socket,
+		user=CLOUDSQL_USER,
+		passwd=CLOUDSQL_PASSWORD,
+		CLOUDSQL_DB)
+
+	return db
+
+db = connect_to_cloudsql()
+
+def checkUser(firstname, lastname, employeeId):
+
+	cursor = db.cursor()
+	cursor.execute("SELECT COUNT(*) FROM UserMaster WHERE FirstName = '%s' AND LastName = '%s' AND EmpCode = '%s'" % (firstname, lastname, employeeId))
+
+	count = cursor.rowcount
+	
+	if count > 0:
+		return True
+	else :
+		return False
